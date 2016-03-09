6cec78ac5be5026f8637845c8ef98ef0c123a0ea
Peter Brumblay <peter@tyrconsulting.com>
2016-03-09 06:41:57 -0500

    removed fat arrow

--- tests.js
+++ tests.js
@@ -16,7 +16,7 @@ test.createStream().on('data', function (row) {
 test('Verify the test application is running', function (t) {
     client
         .get('http://purchase-history-test.apps.10.2.2.2.xip.io/healthcheck')
-        .end((err, res) => {
+        .end(function(err, res) {
             t.plan(2);
             t.ok(res.status === 200, 'Healthcheck responded with 200.');
             t.ok(res.text.indexOf('Up and running') >= 0, 'Content contained "up and running"')
