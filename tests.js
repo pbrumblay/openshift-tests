0553c082854a374c45904bfb2ac1692918a58c47
Peter Brumblay <peter@tyrconsulting.com>
2016-03-09 06:18:12 -0500

    tape back in, service reports errors

--- tests.js
+++ tests.js
@@ -3,19 +3,35 @@
 
 var client = require('superagent');
 var restify = require('restify');
-
+var test = require('tape');
 var server = restify.createServer();
 
-function runTests(req, response, next) {
+//tests
+var results = "";
+test.createStream().on('data', function (row) {
+    results = results + row
+});
+
+
+test('Verify the test application is running', function (t) {
     client
         .get('http://purchase-history-test.apps.10.2.2.2.xip.io/healthcheck')
-        .end(function(err, res) {
-            if(res.status === 200 && res.text.indexOf('Up and running') >= 0) response.send("healthcheck returned success");
-            next();
+        .end((err, res) => {
+            t.plan(2);
+            t.ok(res.status === 200, 'Healthcheck responded with 200.');
+            t.ok(res.text.indexOf('Up and running') >= 0, 'Content contained "up and running"')
         });
+});
+
+//restify endpoint for viewing results.
+function reportResults(req, response, next) {
+    console.log(results);
+    response.setHeader('content-type', 'text/plain');
+    response.send(results);
+    next();
 }
 
-server.get('/runtests', runTests);
+server.get('/', reportResults);
 
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
